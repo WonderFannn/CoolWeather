fddec40bdd9d75e2a7a711950fe5b9cb50aa47d9
wangfan <sail032@live.cn>
2017-04-18 15:39:48 +0800

    添加忽略文件夹

--- app/src/main/java/com/personal/wangfan/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/personal/wangfan/coolweather/activity/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text)
     TextView textView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
